a93a8d1d4a2812e5bfd900144ec0a06ecde59dbb
Nick Breen <nick@foobar.net.nz>
2017-08-14 21:45:14 +1200

    unit

--- autorip.sh
+++ autorip.sh
@@ -228,7 +228,7 @@ cdenc ()
 		
 	logger -p $LOGDEST.info "Info ($PROCESS): Starting encoding."
 	
-	abcde -V -Nx -d $CDROM $VALIDTRACKS > /dev/null 2>&1
+	abcde -Nx -d $CDROM $VALIDTRACKS  2>&1 | logger -p $LOGDEST.info
 		
 
 	# If the disc is unknwon, give a warning and unique folder name.
